d5d90917ef9bad58ff692898d24130842cd4c4cf
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-14 11:57:42 -0700

    Some code cleanup and review

--- manifests/tomcat_application.pp
+++ manifests/tomcat_application.pp
@@ -1,18 +1,20 @@
 define tomcat7_rhel::tomcat_application(
-  $application_name = $title,
+  $application_name         = $title,
   $application_root,
   $tomcat_user,
   $tomcat_port,
-  $tomcat_control_port = ($tomcat_port + 1000),
+  $tomcat_control_port      = ($tomcat_port + 1000),
   $jvm_envs,
-  $tomcat_manager = false,
-  $tomcat_admin_user = "tomcat",
-  $tomcat_admin_password = "s3cr3t",
+  $tomcat_manager           = false,
+  $tomcat_admin_user        = "tomcat",
+  $tomcat_admin_password    = "s3cr3t",
   $server_xml_engine_config = "",
-  $disable_access_log = false,
-  $jmx_registry_port = 10052,
-  $jmx_server_port = 10051,
-  $smoke_test_path = "/") {
+  $disable_access_log       = false,
+  $jmx_registry_port        = 10052,
+  $jmx_server_port          = 10051,
+  $smoke_test_path          = "/"
+) {
+
   include tomcat7_rhel
 
   $application_dir = "$application_root/$application_name"
@@ -23,21 +25,22 @@ define tomcat7_rhel::tomcat_application(
     before => Service["$application_name"]
   }
 
-  file { [
-    "$application_dir/bin","$application_dir/conf","$application_dir/temp",
-    "$application_dir/work", "$application_dir/logs",
-    "$application_dir/webapps", "$application_dir/conf/Catalina",
-    "$application_dir/conf/Catalina/localhost"]:
+  $app_dirs = prefix ([ 
+    "/bin", "/logs", "/temp", "/webapps", "/work",
+    "/conf", "/conf/Catalina", "/conf/Catalina/localhost"
+  ], $application_dir)
+
+  file { $app_dirs:
     ensure => directory,
-    owner => $tomcat_user,
-    group => $tomcat_user
+    owner  => $tomcat_user,
+    group  => $tomcat_user
   }
 
   file { "$application_dir":
     ensure => directory
   }
 
-  service { "$application_name":
+  service { $application_name:
     ensure    => running,
     enable    => true,
     hasstatus => true,
@@ -45,19 +48,19 @@ define tomcat7_rhel::tomcat_application(
   }
 
   file { "/etc/init.d/$application_name":
-    ensure => link,
-    target => "/etc/init.d/tomcat7",
+    ensure  => link,
+    target  => "/etc/init.d/tomcat7",
     require => Package['tomcat7']
   }
 
   if $tomcat_manager == true {
     tomcat7_rhel::tomcat_manager { "Install Tomcat Manager for $application_name":
-    tomcat_admin_user => $tomcat_admin_user,
-    tomcat_admin_password => $tomcat_admin_password,
-    tomcat_user => $tomcat_user,
-    application_dir => $application_dir,
-    application_name => $application_name,
-    tomcat_port => $tomcat_port
+      tomcat_admin_user     => $tomcat_admin_user,
+      tomcat_admin_password => $tomcat_admin_password,
+      tomcat_user           => $tomcat_user,
+      application_dir       => $application_dir,
+      application_name      => $application_name,
+      tomcat_port           => $tomcat_port
     }
   }
 
