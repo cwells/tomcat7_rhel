a2c99b73c664864f1adbbb67512c2fb958a639f9
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-15 14:07:39 -0700

    Ensure tomcat running

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,21 @@
 class tomcat7_rhel {
   include tomcat7_rhel::jpackage_repo
 
-  package { "java-1.7.0-openjdk":
+  package { 'java-1.7.0-openjdk':
     ensure => latest
   }
-  package { "java-1.7.0-openjdk-devel":
+
+  package { 'java-1.7.0-openjdk-devel':
     ensure  => latest,
     require => Package["java-1.7.0-openjdk"]
   }
 
-  package { "tomcat7":
+  package { 'tomcat7':
     ensure  => installed,
     require => [Package['java-1.7.0-openjdk'], Yumrepo['jpp6']]
+  }
+
+  service { ['tomcat7']: 
+    ensure => 'running' 
   }
 }
