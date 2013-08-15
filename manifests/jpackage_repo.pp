dc3464712383b627a8b34b9d453f2a131809c570
Cliff Wells <cliff.wells@rentrakmail.com>
2013-08-15 12:09:13 -0700

    Additional deps allowed from repo

--- manifests/jpackage_repo.pp
+++ manifests/jpackage_repo.pp
@@ -4,6 +4,6 @@ class tomcat7_rhel::jpackage_repo {
     baseurl     => "http://cobbler/cobbler/repo_mirror/jpp6-beta",
     enabled     => 1,
     gpgcheck    => 0,
-    includepkgs => 'apache-commons* jpackage-utils tomcat7*'
+    includepkgs => 'apache-commons* ecj3 jpackage-utils tomcat7*'
   }
 }
